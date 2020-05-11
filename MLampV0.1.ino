9b4a43779c997201f64ef5cc693bc39fa590fa9f
asidar <63718466+asidar@users.noreply.github.com>
2020-05-11 20:42:31 +0530

    Create MLampV0.1.ino
    Very first version of MLamp

---
+++ MLampV0.1.ino
@@ -0,0 +1,116 @@
+/*
+---------------------------------------------------------------------------------------
+Date Created - 11/05/2020 | Version - 0.1 (N00B Level)
+Code Author = https://github.com/asidar
+Learned about FastLED usage from = https://howtomechatronics.com/tutorials/arduino/how-to-control-ws2812b-individually-addressable-leds-using-arduino/
+
+Feel free to suggest new features  / PR!
+---------------------------------------------------------------------------------------
+*/
+
+#include <FastLED.h>
+#define LED_PIN     05
+#define NUM_LEDS    1
+CRGB leds[NUM_LEDS];
+char cr=0;
+char cg=0;
+char cb=0;
+
+void showit(){
+  leds[0]=CRGB(cr,cg,cb);
+  FastLED.show();
+  delay(50); // Change the delay accord
+  //Serial.print("R= ");
+  //Serial.print(cr, DEC);
+  //Serial.print(" | G= ");
+  //Serial.print(cg, DEC);
+  //Serial.print(" | B= ");
+  //Serial.print(cb, DEC);
+  //Serial.println();
+}
+
+void crup(){
+  //Serial.println("\t\tRED Increasing!");
+  for(cr=0;cr<=254;cr++)
+  {
+    showit();
+  }
+}
+
+void cgup(){
+  //Serial.println("\t\tGREEN Increasing!");
+   for(cg=0;cg<=254;cg++)
+  {
+    showit();
+  }
+}
+
+void crdn(){
+  //Serial.println("\t\tRED Decreasing!");
+   for(cr=255;cr>=1;cr--)
+  {
+    showit();
+  }
+}
+
+
+void cgdn(){
+  //Serial.println("\t\tGREEN Decreasing!");
+   for(cg=255;cg>=1;cg--)
+  {
+    showit();
+  }
+}
+
+
+void cbup(){
+  //Serial.println("\t\tBLUE Increasing!");
+  for(cb=0;cb<=254;cb++)
+  {
+    showit();
+  }
+}
+
+
+void cbdn(){
+  //Serial.println("\t\tBLUE Decreasing!");
+   for(cb=255;cb>=1;cb--)
+  {
+    showit();
+  }
+}
+
+
+void setup() {
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  //Serial.begin(9600); //added for debugging purposes.
+
+}
+
+void loop() {
+      crup();
+      
+      cgup();
+     
+      crdn();
+       
+      cbup();
+       
+      cgdn();
+       
+      crup();
+       
+      cbdn();
+       
+      cgup();
+       
+      cbup();
+       
+      crdn();
+       
+      cgdn();
+       
+      cbdn();
+
+      delay(100); //optional delay
+}
