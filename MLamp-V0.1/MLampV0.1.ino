d272d47dfd68e940d80196e95fc0199311f4e75a
asidar <63718466+asidar@users.noreply.github.com>
2020-05-13 22:11:45 +0530

    Update MLampV0.1.ino

--- MLamp-V0.1/MLampV0.1.ino
+++ MLamp-V0.1/MLampV0.1.ino
@@ -1,5 +1,6 @@
 /*
 ---------------------------------------------------------------------------------------
+Project Title = Mood Lamp (Single WS2812B RGB LED based)
 Date Created - 11/05/2020 | Version - 0.1 (N00B Level)
 Code Author = https://github.com/asidar
 Learned about FastLED usage from = https://howtomechatronics.com/tutorials/arduino/how-to-control-ws2812b-individually-addressable-leds-using-arduino/
