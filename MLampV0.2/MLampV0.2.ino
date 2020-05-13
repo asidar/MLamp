528d84839fa86bd85a8e5ec46dafd4ba538dcf41
asidar <63718466+asidar@users.noreply.github.com>
2020-05-13 20:57:05 +0530

    Update MLampV0.2.ino

--- MLampV0.2/MLampV0.2.ino
+++ MLampV0.2/MLampV0.2.ino
@@ -1,1 +1,8 @@
-
+/*
+---------------------------------------------------------------------------------------
+Date Created - 11/05/2020 | Version - 0.2 (Improvised but still at N00B Level)
+Code Author = https://github.com/asidar
+Learned about FastLED usage from = https://howtomechatronics.com/tutorials/arduino/how-to-control-ws2812b-individually-addressable-leds-using-arduino/
+Feel free to suggest new features  / PR!
+---------------------------------------------------------------------------------------
+*/
