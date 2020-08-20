e05353eed77f08d85f45eb5728fe0847ee63965e
pyxit <63718466+pytxt@users.noreply.github.com>
2020-08-20 11:02:32 -0400

    Update MLampV0.2.ino

--- MLampV0.2/MLampV0.2.ino
+++ MLampV0.2/MLampV0.2.ino
@@ -2,7 +2,7 @@
 ---------------------------------------------------------------------------------------
 Project Title = Mood Lamp (Single WS2812B RGB LED based)
 Date Created - 11/05/2020 | Version - 0.2 (Improvised but still at N00B Level)
-Code Author = https://github.com/asidar
+Code Author = https://github.com/pytxt
 Learned about FastLED usage from = https://howtomechatronics.com/tutorials/arduino/how-to-control-ws2812b-individually-addressable-leds-using-arduino/
 Feel free to suggest new features  / PR!
 ---------------------------------------------------------------------------------------
